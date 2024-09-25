e500b4d356cfc0b1e7b3bc8a1886d055fa5d940b
ReynaldiRP <rizkypratama471@gmail.com>
2024-09-25 17:26:13 +0700

    [chore] add configuration for path vue 3 and configuration for vue in vite config

---
+++ resources/js/Layouts/GuestLayout.vue
@@ -0,0 +1,11 @@
+<template>
+    <header></header>
+    <main>
+        <slot></slot>
+    </main>
+    <footer></footer>
+</template>
+
+<script setup></script>
+
+<style lang="scss" scoped></style>
---
+++ resources/js/Pages/Test.vue
@@ -0,0 +1,9 @@
+<template>
+    <guest-layout>
+        <h1>Welcome to inerti laravel 10 + vue 3 starter project</h1>
+    </guest-layout>
+</template>
+
+<script setup>
+import GuestLayout from "@/Layouts/GuestLayout.vue";
+</script>
